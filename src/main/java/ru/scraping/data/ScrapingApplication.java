40d3fd29261236080cb1969ce706e35fc0202d82
Sergey_Shabanov <ShabanovSA@mosreg.ru>
2023-12-27 18:31:11 +0500

    scraping aplication

--- src/main/java/ru/scraping/data/ScrapingApplication.java
+++ src/main/java/ru/scraping/data/ScrapingApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScrapingApplication {
 
-	private ScrapingApplication() {
-
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ScrapingApplication.class, args);
 	}
