82bb94502532f2c9b4ffe9aa1ecf796363d46bcf
Sergey_Shabanov <ShabanovSA@mosreg.ru>
2024-01-05 11:41:00 +0500

    scraping aplication

--- src/main/java/ru/scraping/data/service/impl/HashServiceImpl.java
+++ src/main/java/ru/scraping/data/service/impl/HashServiceImpl.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.scraping.data.exceptions.ServiceException;
 import ru.scraping.data.service.HashService;
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
